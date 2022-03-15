a8d7176fe37dce709d525ef25e149bc72f858f7d
TheK4n <djvlad967891@gmail.com>
2022-03-15 22:25:31 +0300

    ref(tg.py)

--- tg.py
+++ tg.py
@@ -16,24 +16,26 @@ def execute_add_client_sh(path: str, client_name: str):
 
 @bot.message_handler(commands=['add_client'])
 def add_client_handler(message):
-    if message.from_user.id == int(os.getenv("ADMIN")):
+    if message.from_user.id != int(os.getenv("ADMIN")):
+        return
 
-        message_args = message.text.split(" ")
-        if len(message_args) < 2:
-            bot.reply_to(message, "Client name was not defined, use '/add_client <client_name>'")
-            return
+    message_args = message.text.split(" ")
+    if len(message_args) < 2:
+        bot.reply_to(message, "Client name was not defined, use '/add_client <client_name>'")
+        return
 
-        bot.reply_to(message, "Please wait!")
+    bot.reply_to(message, "Please wait!")
 
-        client_name = message_args[1]
-        command_add_client_result = execute_add_client_sh(os.getenv("EXECPATH"), client_name)
+    client_name = message_args[1]
+    command_add_client_result = execute_add_client_sh(os.getenv("EXECPATH"), client_name)
 
-        if command_add_client_result.returncode:
-            logger.error("add_client.sh returned non-zero code")
-            bot.reply_to(message, "Error")
-        else:
-            bot.reply_to(message, command_add_client_result.stdout.decode())
-            logger.info(f"New client '{client_name}' was added")
+    if command_add_client_result.returncode:
+        logger.error("add_client.sh returned non-zero code")
+        bot.reply_to(message, "Error")
+        return
+
+    bot.reply_to(message, command_add_client_result.stdout.decode())
+    logger.info(f"New client '{client_name}' was added")
 
 
 if __name__ == "__main__":
