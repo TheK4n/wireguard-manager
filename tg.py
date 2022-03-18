d7b525c631367116ab0bdfe9e9d34e38f71a5b65
TheK4n <djvlad967891@gmail.com>
2022-03-18 18:13:58 +0300

    add handler get_client to tg bot

--- tg.py
+++ tg.py
@@ -10,7 +10,7 @@ load_dotenv()
 bot = telebot.TeleBot(os.getenv("TOKEN"))
 
 
-def execute_add_client_sh(path: str, client_name: str):
+def execute_sh(path: str, client_name: str):
     return subprocess.run(["bash", path, client_name], capture_output=True)
 
 
@@ -27,15 +27,38 @@ def add_client_handler(message):
     bot.reply_to(message, "Please wait!")
 
     client_name = message_args[1]
-    command_add_client_result = execute_add_client_sh(os.getenv("EXECPATH"), client_name)
+    command_result = execute_sh("scripts/add_client.sh", client_name)
 
-    if command_add_client_result.returncode:
+    if command_result.returncode:
         logger.error("add_client.sh returned non-zero code")
         bot.reply_to(message, "Error")
         return
 
-    bot.reply_to(message, command_add_client_result.stdout.decode())
+    bot.reply_to(message, command_result.stdout.decode())
     logger.info(f"New client '{client_name}' was added")
+
+
+@bot.message_handler(commands=['get_client'])
+def get_client_handler(message):
+    if message.from_user.id != int(os.getenv("ADMIN")):
+        return
+
+    message_args = message.text.split(" ")
+    if len(message_args) < 2:
+        bot.reply_to(message, "Client name was not defined, use '/get_client <client_name>'")
+        return
+
+    bot.reply_to(message, "Please wait!")
+
+    client_name = message_args[1]
+    command_result = execute_sh("scripts/get_client.sh", client_name)
+
+    if command_result.returncode:
+        logger.error("get_client.sh returned non-zero code")
+        bot.reply_to(message, "Error")
+        return
+
+    bot.reply_to(message, command_result.stdout.decode())
 
 
 if __name__ == "__main__":
