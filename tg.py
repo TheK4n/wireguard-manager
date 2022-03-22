3e28cb4b407609d35751f70088244368e3cb99d9
TheK4n <djvlad967891@gmail.com>
2022-03-22 21:57:05 +0300

    add tg commands get_file get_text

--- tg.py
+++ tg.py
@@ -36,7 +36,7 @@ def base_handler(message, command: str):
     command_result = execute_sh("wg_manager.sh", command, client_name)
 
     if command_result.returncode:
-        logger.error(command_result.stderr.decode())
+        logger.error(command_result.stderr.decode().strip())
         bot.edit_message_text(chat_id=first_message.chat.id, message_id=first_message.message_id, text="Error!")
         return
     photo = BytesIO(command_result.stdout)
@@ -49,6 +49,55 @@ def base_handler(message, command: str):
 @bot.message_handler(commands=['add'])
 def add_client_handler(message):
     base_handler(message, "add_tg")
+
+
+@bot.message_handler(commands=['get_text'])
+def get_text_handler(message):
+    if not check_admin(message.from_user.id):
+        return
+    if not check_args(message.text):
+        bot.reply_to(message, "Client name was not defined")
+        return
+
+    first_message = bot.reply_to(message, "Please wait!")
+
+    client_name = message.text.split()[1]
+    command_result = execute_sh("wg_manager.sh", "get_file", client_name)
+
+    if command_result.returncode:
+        logger.error(command_result.stderr.decode().strip())
+        bot.edit_message_text(chat_id=first_message.chat.id, message_id=first_message.message_id, text="Error!")
+        return
+
+    bot.reply_to(message, command_result.stdout.decode())
+    bot.delete_message(chat_id=first_message.chat.id, message_id=first_message.message_id)
+    logger.info(f"command 'get_text' executed")
+
+
+@bot.message_handler(commands=['get_file'])
+def get_file_handler(message):
+    if not check_admin(message.from_user.id):
+        return
+    if not check_args(message.text):
+        bot.reply_to(message, "Client name was not defined")
+        return
+
+    first_message = bot.reply_to(message, "Please wait!")
+
+    client_name = message.text.split()[1]
+    command_result = execute_sh("wg_manager.sh", "get_file", client_name)
+
+    if command_result.returncode:
+        logger.error(command_result.stderr.decode().strip())
+        bot.edit_message_text(chat_id=first_message.chat.id, message_id=first_message.message_id, text="Error!")
+        return
+
+    file = BytesIO(command_result.stdout)
+    file.name = client_name + ".conf"
+    file.seek(0)
+    bot.send_document(message.from_user.id, document=file)
+    bot.delete_message(chat_id=first_message.chat.id, message_id=first_message.message_id)
+    logger.info(f"command 'get_text' executed")
 
 
 @bot.message_handler(commands=['get'])
@@ -64,7 +113,7 @@ def ls_client_handler(message):
     command_result = execute_sh("wg_manager.sh", "ls", "")
 
     if command_result.returncode:
-        logger.error(command_result.stderr.decode())
+        logger.error(command_result.stderr.decode().strip())
         bot.reply_to(message, "Error")
         return
 
@@ -86,7 +135,7 @@ def get_client_handler(message):
     command_result = execute_sh("wg_manager.sh", "rm", client_name)
 
     if command_result.returncode:
-        logger.error(command_result.stderr.decode())
+        logger.error(command_result.stderr.decode().strip())
         bot.reply_to(message, "Error")
         return
 
