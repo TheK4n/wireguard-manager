d2e96c1f91d8121bc579b408e5d62c1c85781961
TheK4n <djvlad967891@gmail.com>
2022-03-23 15:28:20 +0300

    state

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -8,21 +8,21 @@ from keyboards import menu
 
 @dp.message_handler(CommandStart(), state='*', user_id=ADMINS)
 async def bot_start(message: types.Message):
-    # state = dp.current_state(user=message.from_user.id)
-    # await state.reset_state()
+    state = dp.current_state(user=message.from_user.id)
+    await state.reset_state()
     logger.info(f"{message.text} from user {message.from_user.username}:{message.from_user.id}")
     await message.answer(f"Hello, Access allowed to {message.from_user.full_name}", reply_markup=menu)
 
 
 @dp.message_handler(CommandHelp(), state='*', user_id=ADMINS)
 async def bot_help(message: types.Message):
-    # state = dp.current_state(user=message.from_user.id)
-    # await state.reset_state()
+    state = dp.current_state(user=message.from_user.id)
+    await state.reset_state()
     await message.answer("WireGuard Manager bot\n\nGithub: https://github.com/thek4n/wireguard-manager")
 
 
 @dp.message_handler(Command("menu"), state='*', user_id=ADMINS)
 async def bot_menu(message: types.Message):
-    # state = dp.current_state(user=message.from_user.id)
-    # await state.reset_state()
+    state = dp.current_state(user=message.from_user.id)
+    await state.reset_state()
     await message.answer("WireGuard Manager bot menu", reply_markup=menu)
